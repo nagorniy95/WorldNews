a7f085dd702a155a24229cb243053f6de9090f23
Artem Nahornyi <nagornyi95@gmail.com>
2019-05-26 22:43:02 -0400

    add new crypto article

--- root/views/Crypto/addCryptoNews.php
+++ root/views/Crypto/addCryptoNews.php
@@ -2,7 +2,7 @@
 <?php
 session_start();
 $page_title = "AddCryptoNews";
-
+// ============ HEADER ==============
 include "../../views/header.php"; 
 require_once '../../model/Database.php';
 require_once '../../model/Crypto.php';
@@ -22,27 +22,30 @@ if(isset($_POST['addnews'])){
 }
 ?>
 <div class="container">
-    <h3>Add new Article</h3>
+    <h3 align="center">Add new Article</h3><br/>
     <form action="" method="POST" enctype="multipart/form-data">
         <label for="crypto_title">Title:</label><br/>
-        <input type="text" name="title" id="crypto_title" required placeholder="Title..." /><br/>
+        <input type="text" name="title" id="crypto_title" required placeholder="Title..." class="form-control" /><br/>
 
         <label for="crypto_category">Category:</label><br/>
-        <input type="text" name="crypto_category" id="crypto_category" required placeholder="Category..." /><br/>
+        <input type="text" name="crypto_category" id="crypto_category" required placeholder="Category..." class="form-control" /><br/>
 
         <label for="crypto_author">Author:</label><br/>
-        <input type="text" name="crypto_author" id="crypto_author" placeholder="Author..." /><br/>
+        <input type="text" name="crypto_author" id="crypto_author" placeholder="Author..." class="form-control" /><br/>
 
         <label for="crypto_article">Article:</label><br/>
-        <textarea type="text" name="crypto_article" id="crypto_article" placeholder="Enter text here..." required rows="10" cols="80"></textarea><br/>
+        <textarea type="text" name="crypto_article" id="crypto_article" placeholder="Enter text here..." required rows="10" cols="80" class="form-control"></textarea><br/>
 
         <label for="crypto_image_title">Image Title:</label><br/>
-        <input type="text" name="crypto_image_title" id="crypto_image_title" placeholder="Imgage title..."><br/>
+        <input type="text" name="crypto_image_title" id="crypto_image_title" placeholder="Image title..." class="form-control" /><br/>
 
         <label for="crypto_image">Image:</label><br/>
         <input type="file" name="image" id="crypto_image"><br/>
 
-        <input type="Reset" name="Reset" value="Reset" />
-        <input type="submit" name="addnews" value="Submit" />
+        <input type="Reset" name="Reset" value="Reset" class="btn btn-default" />
+        <input type="submit" name="addnews" value="Submit" class="btn btn-default" />
     </form>
 </div>
+<!-- ============ Footer ============ -->
+<?php include "../../views/footer.php"; ?>
+
