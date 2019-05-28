8b2e691f65e25f8228dfccc52e27cd18c358c182
Artem Nahornyi <nagornyi95@gmail.com>
2019-05-27 21:50:57 -0400

    Crypto News Delete

--- root/model/Crypto.php
+++ root/model/Crypto.php
@@ -9,6 +9,7 @@ class Crypto
         $pst->bindParam(':id', $id);
         $pst->execute();
         $result =  $pst->fetch(PDO::FETCH_OBJ);
+
         return $result;
     }
     
@@ -20,6 +21,7 @@ class Crypto
         $pdostm = $dbcon->prepare($sql); // prepare
         $pdostm->execute(); // execute
         $result = $pdostm->fetchAll(PDO::FETCH_OBJ);
+
         return $result;
     }
 
@@ -43,6 +45,47 @@ class Crypto
         $result = $pst->execute();
 
         return $result;
+    }
+
+    public function updateCrypto($id, $title, $category, $author, $article, $image_title, $db){
+        $sql = "UPDATE crypto_news
+                SET title = :title,
+                category = :category,
+                author = :author,
+                article = :article,
+                WHERE id = :id";
+        $pst = $db->prepare($sql);
+        $pst->bindParam(':id', $id);
+        $pst->bindParam(':title', $title);
+        $pst->bindParam(':category', $category);
+        $pst->bindParam(':author', $author);
+        $pst->bindParam(':article', $article);
+        $result = $pst->execute();
+
+        // image update-------------------------------
+        // $sql = "UPDATE crypto_news_images
+        //         SET file = :img
+        //         WHERE product_id = :id";
+        // $pst = $db->prepare($sql);
+        // $pst->bindParam(':img', $img);
+        // $pst->bindParam(':id', $id);
+        // $result = $pst->execute();
+
+        return $result;
+    }
+
+    public function deleteCrypto($id, $db){
+        $sql = "DELETE  FROM crypto_news_images WHERE id = :id";
+        $pst = $db->prepare($sql);
+        $pst->bindParam(':id', $id);
+        $count = $pst->execute();
+
+        $sql = "DELETE FROM crypto_news WHERE id = :id";
+        $pst = $db->prepare($sql);
+        $pst->bindParam(':id', $id);
+        $count = $pst->execute();
+
+        return $count;
     }
     
 } // end Crypto class
--- root/views/Crypto/crypto-admin.php
+++ root/views/Crypto/crypto-admin.php
@@ -38,8 +38,9 @@ if(isset($_POST['addnews'])){
 }
 ?>
 <div class="container">
+    <h2 align="center">ADMIN</h2>
     <h3 align="center">Add new Article</h3><br/>
-    <form action="" method="POST" enctype="multipart/form-data">
+    <form action="#" method="POST" enctype="multipart/form-data">
         <label for="crypto_title">Title:</label><br/>
         <input type="text" name="crypto_title" id="crypto_title" required placeholder="Title..." class="form-control" /><br/>
 
@@ -50,7 +51,7 @@ if(isset($_POST['addnews'])){
         <input type="text" name="crypto_author" id="crypto_author" placeholder="Author..." class="form-control" /><br/>
 
         <label for="crypto_article">Article:</label><br/>
-        <textarea type="text" name="crypto_article" id="crypto_article" placeholder="Enter text here..." required rows="10" cols="80" class="form-control"></textarea><br/>
+        <textarea type="text" name="crypto_article" id="crypto_article" placeholder="Enter text here..." required rows="8" class="form-control"></textarea><br/>
 
         <label for="crypto_image_title">Image Title:</label><br/>
         <input type="text" name="crypto_image_title" id="crypto_image_title" placeholder="Image title..." class="form-control" /><br/>
@@ -59,9 +60,32 @@ if(isset($_POST['addnews'])){
         <input type="file" name="crypto_image" id="crypto_image"><br/>
 
         <input type="Reset" name="Reset" value="Reset" class="btn btn-default" />
-        <input type="submit" name="addnews" value="Submit" class="btn btn-default" />
-    </form>
-</div>
+        <input type="submit" name="addnews" value="Submit" class="btn btn-default" /><br/><br/>
+        <input type="button" value="Go Back to Crypto News" class="btn btn-default" onClick="document.location.href='../Crypto/crypto.php'" />    </form><hr>
+<!-- ==================================== Author NEWS ==================================== -->
+<!-- ===================================================================================== -->
+<?php $c = new Crypto(); $mynews = $c->getAllCrypto(Database::getDb()); ?>
+    <div class="row">
+        <h3 align="center">Crypto News List</h3><br>
+        <?php foreach($mynews as $news){ ?>
+        <div class='col-md-4 col-sm-6 col-12' align="center">
+            <img class="crypto_news_image" src="images/<?php echo $news->file; ?>" height="250px">
+            <h4><a href="cryptoArticle.php?id=<?php echo $news->id; ?>"><?php echo $news->title; ?></a></h4>
+            <div class="row">
+                <form action='updateCrypto.php' method='POST' class="col-md-6">
+                    <input type='hidden' value='<?= $news->id; ?>' name='id' />
+                    <input type='submit' value='Update' name='update' class="btn btn-primary" />
+                </form>
+                <form action='deleteCrypto.php' method='POST' class="col-md-6">
+                    <input type='hidden' value='<?= $news->id; ?>' name='id' />
+                    <input type='submit' value='Delete' name='delete' class="btn btn-danger" onclick="return confirm('Are you sure to delete?')" />
+                </form>
+            </div>
+        </div>
+        <?php }; ?>
+    </div>
+</div><!-- end container -->
+
 
 <!-- ============ Footer ============ -->
 <?php include "../../views/footer.php"; ?>
