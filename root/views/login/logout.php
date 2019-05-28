3411590b9aa50bcadec7fee4993da7ee507b0d64
Asus Laptop <Asus Laptop@RAYRAY>
2019-05-28 08:49:12 -0400

    added login part Rabih aoun

---
+++ root/views/login/logout.php
@@ -0,0 +1,11 @@
+<?php
+include('Database.php');
+$session_id='';
+$_SESSION['id']=''; 
+if(empty($session_id) && empty($_SESSION['id']))
+{
+$url='index.php';
+header("Location: $url");
+//echo "<script>window.location='$url'</script>";
+}
+?>
---
+++ root/views/login/register.php
@@ -0,0 +1,41 @@
+<?php include('functions.php') ?>
+
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" href="login.css">
+
+<!-- Bootstrap CDN -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="header">
+	<h2>Register</h2>
+</div>
+<form method="post" action="register.php">
+	<?php echo display_error(); ?>
+	<div class="input-group">
+		<label>Username</label>
+		<input type="text" name="username" value="<?php echo $username; ?>">
+	</div>
+	<div class="input-group">
+		<label>Email Address</label>
+		<input type="email" name="email" value="<?php echo $email; ?>">
+	</div>
+	<div class="input-group">
+		<label>Password</label>
+		<input type="password" name="password_1">
+	</div>
+	<div class="input-group">
+		<label>Confirm password</label>
+		<input type="password" name="password_2">
+	</div>
+	<div class="input-group d-flex justify-content-center">
+		<button type="submit" class="btn" name="register_btn">Register</button>
+	</div>
+	<p>
+		Already a member? <a href="login.php">Sign in</a>
+	</p>
+</form>
+</body>
+</html>
