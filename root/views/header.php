174995905ad79ade859fe6a42b1eb986ac5474c9
Asus Laptop <Asus Laptop@RAYRAY>
2019-05-31 13:19:21 -0400

    added header&footer

--- root/views/header.php
+++ root/views/header.php
@@ -21,5 +21,41 @@
 
 <body>
     <header id="header">
-        
+        <nav class="navbar navbar-expand-lg static-top" style="background-color: #FFFFFF!important; color: white">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="../../source/logo.png" alt="" class="header-logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Sport</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Technology</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Crypto Currency</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Finance</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </header>
--- root/views/index.php
+++ root/views/index.php
@@ -7,6 +7,11 @@ include "../views/header.php";
 ?>
 <!--  ==================================== MAIN CONTENT AREA ==================================== -->
 
-
+<h1>World News</h1>
+<h2>World News</h2>
+<h3>World News</h3>
+<h4>World News</h4>
+<h5>World News</h5>
+<h6>World News</h6>
 <!-- ==================================== FOOTER ==================================== -->
 <?php include "../views/footer.php"; ?>
