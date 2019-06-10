a327c144b7d699843404e5c7e56512b1de08091c
maria <ko.marik@meta.ua>
2019-06-10 14:21:17 -0400

    add about page

---
+++ root/views/About/index.php
@@ -0,0 +1,33 @@
+<?php
+$page_title = "About Us";
+include dirname( __FILE__) . "../../header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+<img class="img-responsive" src="../../img/about-img.jpeg">
+
+        <div class="about-div">
+<p>
+The World News is the world’s leading public service broadcaster
+We’re impartial and independent, and every day we create distinctive, world-class programmes and content which inform, educate and entertain millions of people in the UK and around the world.We do this across:
+A portfolio of television services, including the UK’s most-watched channel, the pioneering online-only youth service BBC Three, and our multi award-winning channels for children, as well as national 
+and regional television programmes and services across England. Northern Ireland, Scotland and Wales
+Ten UK-wide radio networks, providing the best live music broadcasting in the UK, as well as speech radio which informs, educates and entertains. We also have two national radio services each in Scotland, 
+Wales and Northern Ireland and 39 local radio stations across England and the Channel Islands, providing an invaluable and unique service to listeners across the UK
+Established by a Royal Charter, the World News is principally funded through the licence fee paid by UK households. Our role is to fulfil our mission and promote our Public Purposes.Our commercial operations 
+including BBC Studios, the BBC’s award-winning production company and world-class distributor, provide additional revenue for investment in new programming and services for UK audiences.The BBC’s Board ensures that we deliver our mission and public purposes which are set out in the Charter. The Executive Committee is responsible for day-to-day management. We are regulated by Ofcom.
+</p>
+</div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<?php
+    include dirname( __FILE__) . "../../footer.php";
+?>
